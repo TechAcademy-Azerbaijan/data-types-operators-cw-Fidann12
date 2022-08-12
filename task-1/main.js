ba8a88185db3ae3639582909fef2538135fa8496
Fidann12 <fidan.aliyeva03@icloud.com>
2022-08-12 10:30:28 +0400

    homework

--- task-1/main.js
+++ task-1/main.js
@@ -4,6 +4,13 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+let input = result.input;
+let number = input.split(',');
+let n = parseInt(number[0]);
+let m = parseInt(number[1]);
+let b = parseInt(number[2]);
+let cvb = (n**m)%b
+console.log(cvb);
+
+
 });
